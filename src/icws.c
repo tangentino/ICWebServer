2d8eae460c98d0be760c7042b7dc00a47790c453
Tangentino <tankongpithaksilp@gmail.com>
2021-06-12 19:07:36 +0700

    I hate this class pls take me back to python and java

--- src/icws.c
+++ src/icws.c
@@ -20,6 +20,89 @@ typedef struct sockaddr SA;
 char * port;
 char * root;
 
+
+char * errorMessage(char* buf, char* msg) {
+    // Error response
+    sprintf(buf, 
+        "HTTP/1.1 %s\r\n"
+        "Server: ICWS\r\n"
+        "Connection: close\r\n\r\n",
+        msg
+    );
+    return buf;
+}
+
+char * responseMessage(char* buf, unsigned long size, char* mime) {
+    sprintf(buf,
+        "HTTP/1.1 200 OK\r\n"
+        "Server: ICWS\r\n"
+        "Connection: close\r\n"
+        "Content-Length: %lu\r\n"
+        "Content-Type: %s\r\n\r\n",
+        size, mime
+    );
+    return buf;
+}
+
+char * getMIMEType(char* extension) {
+    // Convert file extension to MIME
+    if (strcmp(extension,"html") == 0) {
+        return "text/html";
+    }
+    else if (strcmp(extension,"css") == 0) {
+        return "text/css";
+    }
+    else if (strcmp(extension,"plain") == 0) {
+        return "text/plain";
+    }
+    else if (strcmp(extension,"javascript") == 0 || strcmp(extension,"js") == 0) {
+        return "text/javascript";
+    }
+    else if (strcmp(extension,"jpg") == 0 || strcmp(extension,"jpeg") == 0) {
+        return "image/jpg";
+    }
+    else if (strcmp(extension,"png") == 0) {
+        return "image/png";
+    }
+    else if (strcmp(extension,"gif") == 0) {
+        return "image/gif";
+    }
+    return "";
+}
+
+void respond(int connFd, char* url, char* mime, char* method) {
+    if ((strcasecmp(method,"GET") != 0) && strcasecmp(method,"HEAD") != 0) {
+        // Method is not GET or HEAD
+        errorMessage(buf, "501 Method Unimplemented");
+        write_all(connFd, buf, strlen(buf));
+
+    }
+    else {
+        char buf[MAXBUF];
+        int urlFd = open(url, O_RDONLY);
+        
+        if (urlFd < 0) {
+            errorMessage(buf, "404 Not Found");
+            write_all(connFd, buf, strlen(buf));
+            return;
+        }
+        struct stat fstatbuf;
+        fstat(urlFd, &fstatbuf);
+        responseMessage(buf,fstat.st_size,mime);
+        write_all(connFd, buf, strlen(buf));
+
+        if (strcasecmp(method,"GET") == 0) {
+            // If method is GET server also returns content body
+            ssize_t numRead;
+            while ((numRead = read(urlFd, buf, MAXBUF)) > 0) {
+                write_all(connFd, buf, numRead);
+            }
+        }
+
+        close(urlFd);
+    }
+}
+
 void serveHttp(int connFd, char *rootFolder) {
 
     char buf[MAXBUF];
@@ -27,22 +110,25 @@ void serveHttp(int connFd, char *rootFolder) {
     Request *request = parse(buf,MAXBUF,connFd);
 
     char method[MAXBUF] = request->http_method;
-    char url[MAXBUF] = request->http_url;
-    char version[MAXBUF] = request->http_version;
+    char url[MAXBUF];
+    //char version[MAXBUF] = request->http_version;
 
-    char newPath[80];
+    char * fileExtension;
+    char * mime;
+    
+    // Url is <root>/<uri>
+    strcpy(url,rootFolder);
+    strcat(url,request->http_uri);
 
-    if (strcasecmp(method,"GET") == 0) {
+    fileExtension = strrchr(url,'.'); // Get file extension in url
+    fileExtension++; // Increment pointer
 
-    }
-    else {
-        char *msg = "501 Method Unimplemented";
-        write_all(connFd, msg, strlen(msg));
-    }
+    mime = getMIMEType(fileExtension);
+
+    respond(connFd, url, mime, method);
 }
 
 void parseArgument(int argc, char **argv) {
-
     // Parsing command line argument to get port and root values
     static struct option longOptions[] = {
         {"port",required_argument,NULL,'p'},
